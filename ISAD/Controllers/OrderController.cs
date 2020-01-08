40ad25ee6f008e49b509e68ae068b3dc5468a127
ohamilton783@gmail.com <ohamilton783@gmail.com>
2020-01-08 17:17:56 +0000

    Everything but sending it to db

--- ISAD/Controllers/OrderController.cs
+++ ISAD/Controllers/OrderController.cs
@@ -13,48 +13,69 @@ namespace ISAD.Controllers
     public class OrderController : Controller
     {
         private readonly ISAD251_OHamiltonContext _context;
+        private IEnumerable<SelectListItem> ProductList;
+        private static List<SelectListItem> OrderingList = new List<SelectListItem> { };
+        private static double price = 0; 
         public OrderController(ISAD251_OHamiltonContext context)
         {
             _context = context;
-        }
+            ProductList = (from p in _context.Products.AsEnumerable()
+                                                               select new SelectListItem
+                                                               {
+                                                                   Text = p.Name,
+                                                                   Value = p.Id.ToString()
+                                                               }).ToList();
+    }
         public IActionResult Index()
         {
-            IEnumerable<SelectListItem> ProductList = (from p in _context.Products.AsEnumerable()
-                                                       select new SelectListItem
-                                                       {
-                                                           Text = p.Name,
-                                                           Value = p.Id.ToString()
-                                                       }).ToList();
-            List<SelectListItem> OrderingList = new List<SelectListItem> { };
             ViewBag.Products = ProductList;
-            ViewBag.Ordering = OrderingList;
             return View();
         }
 
-        public IActionResult AddToOrder()
-        {
-            List<SelectListItem> moreOrders = new List<SelectListItem> { };
-            Console.WriteLine(Request.Form["orderQuantity"]);
-            for (int i = 0; i < Int32.Parse(Request.Form["orderQuantity"]);  i++)
+        public IActionResult EditOrder()
+        {            
+            if (Request.Form.Keys.Contains("Add"))
             {
-                
-                moreOrders.Add(new SelectListItem { Text = _context.Products.Find(Int32.Parse(Request.Form["itemOrdered"])).Name, Value = Request.Form["itemOrdered"]});
+                if (Request.Form.Keys.Contains("itemOrdered"))
+                {
+                    for (int i = 0; i < Int32.Parse(Request.Form["orderQuantity"]); i++)
+                    {
+                        price += (double)_context.Products.Find(Int32.Parse(Request.Form["itemOrdered"])).Price;
+                        OrderingList.Add(new SelectListItem { Text = _context.Products.Find(Int32.Parse(Request.Form["itemOrdered"])).Name, Value = Request.Form["itemOrdered"] });
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = "Please select something to add to your order and try again";
+                }
             }
-            if (ViewBag.Ordering is null)
+            else if (Request.Form.Keys.Contains("Remove"))
             {
-                ViewBag.Ordering = moreOrders;
-            }
-            else
-            {
-                ViewBag.Ordering.Concat(moreOrders);
+                if (Request.Form.Keys.Contains("itemSelected"))
+                {
+                    bool found = false;
+                    int index = 0;
+                    while (!found)
+                    {
+                        if (OrderingList[index].Value == Request.Form["itemSelected"])
+                        {
+                            OrderingList.RemoveAt(index);
+                            found = true;
+                        }
+                        index++;
+                    }
+                    price -= (double)_context.Products.Find(Int32.Parse(Request.Form["itemSelected"])).Price;
+
+                }
+                else
+                {
+                    ViewBag.Error = "Please select something to remove and try again";
+                }
             }
 
-            ViewBag.Products = (from p in _context.Products.AsEnumerable()
-                                select new SelectListItem
-                                {
-                                    Text = p.Name,
-                                    Value = p.Id.ToString()
-                                }).ToList();
+            ViewBag.Price = price;
+            ViewBag.Products = ProductList;
+            ViewBag.Ordering = OrderingList;
             return View("Index");
         }
     }

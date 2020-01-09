8e63a0f1c4c56b26856e5f79aefca2c6047753bd
ItsMeOl1 <44836648+ItsMeOl1@users.noreply.github.com>
2020-01-09 22:11:35 +0000

    Delete SQLAddProducts.sql

--- docs/SQLAddProducts.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Products (Name, ProductDetails, Price, Quantity)
-VALUES ('Tea', 'Its tea', 1.5, 100);
-
-INSERT INTO Products (Name, ProductDetails, Price, Quantity)
-VALUES ('Coffee', 'Its coffee', 2, 50);

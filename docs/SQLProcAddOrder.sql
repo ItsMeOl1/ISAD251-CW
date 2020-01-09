eedfb2de938f360b7b5bdef6a054d58ff54fbe6b
ItsMeOl1 <44836648+ItsMeOl1@users.noreply.github.com>
2020-01-09 22:12:49 +0000

    Delete SQLProcAddOrder.sql

--- docs/SQLProcAddOrder.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE AddOrder @Name nvarchar(255), @TableNumber int, @Price float
-AS
-INSERT INTO Orders (Name, TableNumber, TotalPrice)
-VALUES (@Name, @TableNumber, @Price);
-SELECT SCOPE_IDENTITY()
-RETURN ;

3e36d818a44e6f89c7ee270be962b22599c2c045
ItsMeOl1 <44836648+ItsMeOl1@users.noreply.github.com>
2020-01-09 22:12:37 +0000

    Delete SQLEditProducts.sql

--- docs/SQLEditProducts.sql
+++
@@ -1,5 +0,0 @@
-CREATE PROCEDURE EditProduct @ID int, @Name varchar(255), @Details varchar(255), @Price float, @Quantity INT
-AS
-UPDATE Products
-SET Name = @Name, ProductDetails = @Details, Price = @Price, Quantity = @Quantity
-WHERE ID = @ID;
